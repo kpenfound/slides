4b48d3ab6b5b29801aa96c7908f2fdde790af1b8
kpenfound <kyle@dagger.io>
2025-08-22 17:00:39 -0400

    add dagger module
    Signed-off-by: kpenfound <kyle@dagger.io>

---
+++ .dagger/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"dagger/slides/internal/dagger"
+)
+
+type Slides struct{}
+
+// Run the slides
+func (m *Slides) Slides(
+	ctx context.Context,
+	// presentation file, e.g. self_healing_ci.md
+	presentation string,
+	// +defaultPath="/"
+	source *dagger.Directory,
+) (string, error) {
+	return dag.Container().From("cgr.dev/chainguard/wolfi-base:latest").
+		With(withSlides).
+		With(withMermaidAscii).
+		WithWorkdir("/src").
+		WithDirectory("/src", source).
+		Terminal(
+			dagger.ContainerTerminalOpts{
+				Cmd: []string{"slides", presentation},
+			},
+		).
+		Stderr(ctx)
+}
+
+func withSlides(ctr *dagger.Container) *dagger.Container {
+	slides := dag.Git("https://github.com/maaslalani/slides.git").Head().Tree()
+	build := dag.Container().
+		From("golang:latest").
+		WithDirectory("/app", slides).
+		WithWorkdir("/app").
+		WithExec([]string{"go", "build", "-o", "slides"})
+	return ctr.WithFile("/bin/slides", build.File("/app/slides"))
+}
+
+func withMermaidAscii(ctr *dagger.Container) *dagger.Container {
+	slides := dag.Git("https://github.com/AlexanderGrooff/mermaid-ascii.git").Head().Tree()
+	build := dag.Container().
+		From("golang:latest").
+		WithDirectory("/app", slides).
+		WithWorkdir("/app").
+		WithExec([]string{"go", "build", "-o", "mermaid-ascii"})
+	return ctr.WithFile("/bin/mermaid-ascii", build.File("/app/mermaid-ascii"))
+}
--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 These are the slides for my talks.
 
+Run these slides without any dependencies using [Dagger](https://dagger.io)!
+
+```
+dagger call slides --presentation self_healing_ci.md
+```
+
+or without cloning the repo:
+
+```
+dagger -m github.com/kpenfound/slides call slides --presentation self_healing_ci.md
+```
+
 Slides in markdown files are run with [github.com/maaslalani/slides](https://github.com/maaslalani/slides)
 
 Mermaid diagrams are rendered with [github.com/AlexanderGrooff/mermaid-ascii](https://github.com/AlexanderGrooff/mermaid-ascii)
@@ -10,7 +22,15 @@ Mermaid diagrams are rendered with [github.com/AlexanderGrooff/mermaid-ascii](ht
 
 - [slides](./self_healing_ci.md)
 
+```
+dagger -m github.com/kpenfound/slides call slides --presentation self_healing_ci.md
+```
+
 ## 03.07.2025 - Getting Started with GitHub Actions and Dagger
 
 - [slides](./gha_intro.md)
 - [youtube video](https://www.youtube.com/watch?v=Ahg-3_Ok-rY)
+
+```
+dagger -m github.com/kpenfound/slides call slides --presentation gha_intro.md
+```
